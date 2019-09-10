048cd2ae4d5b291ddcfddc51e34fac7620eaaeae
purerosefallen <78877@qq.com>
2019-09-10 21:49:00 +0800

    add motd

--- Dockerfile
+++ Dockerfile
@@ -4,9 +4,9 @@ FROM node:12-stretch-slim
 # apt
 #COPY sources.list /etc/apt/sources.list
 RUN apt update && \
-    env DEBIAN_FRONTEND=noninteractive apt install -y git wget build-essential libevent-dev libsqlite3-dev mono-complete p7zip-full
-
-RUN useradd -ms /bin/bash container
+    env DEBIAN_FRONTEND=noninteractive apt install -y git wget build-essential libevent-dev libsqlite3-dev mono-complete p7zip-full && \
+    rm -rf /var/lib/apt/lists/* && \ 
+    useradd -ms /bin/bash container
 
 USER container
 ENV  USER container
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,29 +1,8 @@
 #!/bin/bash
 cd /home/container
 
-# Output Current Java Version
-node -v ## only really needed to show what version is being used. Should be changed for different applications
-
-#cd /home/container/windbot
-#git fetch origin master
-#git reset --hard FETCH_HEAD
-#xbuild /property:Configuration=Release /property:TargetFrameworkVersion=v4.5
-
-#cd /home/container/ygopro
-#git fetch origin server
-#git reset --hard FETCH_HEAD
-#git submodule foreach git fetch origin master
-#git submodule foreach git reset --hard FETCH_HEAD
-#./premake5 gmake
-#cd build
-#make config=release
-#cd ..
-#strip ygopro
-
-#cd /home/container/ygopro-server
-#git fetch origin master
-##git reset --hard FETCH_HEAD
-#npm install
+# motd
+curl https://minio.mycard.moe:7900/mycard/srvpro/motd.txt
 
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
